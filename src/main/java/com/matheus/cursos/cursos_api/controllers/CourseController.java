cf813ce7f400fff6f340a7777660ab115a859ddf
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2025-06-25 08:52:25 -0300

    feat: add OpenAPI configuration and enhance CourseController with Swagger annotations

--- src/main/java/com/matheus/cursos/cursos_api/controllers/CourseController.java
+++ src/main/java/com/matheus/cursos/cursos_api/controllers/CourseController.java
@@ -1,6 +1,15 @@
 package com.matheus.cursos.cursos_api.controllers;
 
+import com.matheus.cursos.cursos_api.model.dto.CourseResponseDTO;
 import com.matheus.cursos.cursos_api.services.CourseService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cursos")
+@Tag(name = "Cursos", description = "Operações relacionadas a cursos")
 public class CourseController {
     @Autowired
     private CourseService courseService;
 
     @GetMapping()
+    @Operation(summary = "Listar cursos", description = "Retorna a lista de todos os cursos cadastrados")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Lista de cursos retornada com sucesso", content = {
+            @Content(schema = @Schema(implementation = CourseResponseDTO.class))
+        }),
+    })
     public ResponseEntity<Object> fetchCourse() {
         try {
             var courses = this.courseService.fetch();
